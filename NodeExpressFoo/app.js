a7cf2631a9affb832a6f279428c89866748b1793
Jason Liu <liu.canada@gmail.com>
2012-07-21 21:24:57 -0400

    Network features all functional.

--- NodeExpressFoo/app.js
+++ NodeExpressFoo/app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , _=require('underscore');
 
 var app = module.exports = express.createServer();
 var io = require('socket.io').listen(app);
@@ -62,20 +63,23 @@ io.sockets.on('connection', function(socket){
 		socket.emit('quest', {quest:quest.quest});
 	});
 	socket.on('answer', function(data){
-		console.log('answer: '+data.answer);
-		socket.get('game',function(err, questData){
-			var isCorrect=false;
-			if(questData.answer==data.answer){
-				isCorrect=true;
-				questData.correctCount++;
-			}
-			var quest= generateQuest(questData.operations, questData.difficulty);
-			console.log(quest);
-			questData.quest=quest.quest;
-			questData.answer=quest.answer;
-			socket.set('game',questData);
-			socket.emit('quest', {isCorrect:isCorrect, quest:quest.quest});
-		});
+		
+		multiPlayer.checkAnswer(socket, data.answer);
+		///////////////
+//		console.log('answer: '+data.answer);
+//		socket.get('game',function(err, questData){
+//			var isCorrect=false;
+//			if(questData.answer==data.answer){
+//				isCorrect=true;
+//				questData.correctCount++;
+//			}
+//			var quest= generateQuest(questData.operations, questData.difficulty);
+//			console.log(quest);
+//			questData.quest=quest.quest;
+//			questData.answer=quest.answer;
+//			socket.set('game',questData);
+//			socket.emit('quest', {isCorrect:isCorrect, quest:quest.quest});
+//		});
 	});
 	socket.on('getResult', function(data){
 		socket.get('game', function(err, questData){
@@ -92,25 +96,238 @@ io.sockets.on('connection', function(socket){
 		});
 		
 	});
+	
+	socket.on('joinRoom', function(player){
+		player.socket=socket;
+		multiPlayer.newPlayer(player);
+	});
+	socket.on('ready', function(){
+		multiPlayer.playerReady(socket);
+	});
+	socket.on('leaveRoom', function(player){
+		player.socket=socket;
+		multiPlayer.removePlayer(player);
+	});
+	socket.on('disconnect', function(player){
+		player.socket=socket;
+		multiPlayer.removePlayer(player);
+	});
+	
 });
 
-var gameLobby={
-	players:[],
+var multiPlayer={
+	newPlayer:function(p){
+		console.log(p.name+ ' player clicked "join room".');
+		console.log(player);
+		//Check if the socket is already in the allPlayers list, if yes, remove that one, and create a new player
+		var p1=_.find(gameLobby.allPlayers, function(p1){
+			return p1.id===p.socket.id;
+		});
+		if(p1){
+			this.removePlayer(p1);
+		}
+		var p1=new player(p.name, p.socket.id, p.socket, p.difficulty);
+		gameLobby.freePlayers.push(p1);
+		gameLobby.allPlayers.push(p1);
+		console.log(gameLobby.freePlayers.length+" free players");
+		gameLobby.allocatePlayers();
+	},
+	removePlayer:function(p){
+		_.each(gameLobby.allPlayers,function(p1,key){
+			if(p1.socket===p.socket){
+				gameLobby.allPlayers.splice(key,1);
+				p1.gameRoom.removePlayer(p1);
+				return;
+			};
+		});
+	},
+	playerReady:function(socket){
+		var p=_.find(gameLobby.allPlayers,function(p1){
+			return p1.id==socket.id;
+		});
+		_.each(p.gameRoom.players, function(p1, key){
+			//tell other players that this player is ready.
+			if(p1.id!=socket.id){
+				p1.socket.emit('playerReady', {id:socket.id});
+			}
+		});
+		if(p){
+			p.ready=true;
+			p.gameRoom.prepareStartGame();
+		}
+	},
+	checkAnswer:function(socket, answer){
+		var p=_.find(gameLobby.allPlayers, function(p1){
+			return p1.id===socket.id;
+		});
+		var isCorrect=false;
+		console.log(p.gameRoom.quests);
+		console.log(p.questIndex);
+		if(p.gameRoom.quests[p.questIndex-1].answer==answer){
+			p.correctCount++;
+			isCorrect=true;
+		}
+		var q=p.gameRoom.getQuest(p);
+		//game room announce player status.
+		p.socket.emit('quest', {isCorrect:isCorrect, quest:q});
+		p.gameRoom.announceStatus(p, isCorrect);
+		
+		
+	},
+};
+gameLobby={
+	freePlayers:[],
+	allPlayers:[],
 	gameRoomEasy:[],
 	gameRoomNormal:[],
-	gameRoomHard:[]
+	gameRoomHard:[],
+	allocatePlayers:function(){
+		//add free players to rooms
+		_.each(this.freePlayers, function(player, key){
+			console.log('freePlayers: '+player.difficulty);
+			console.log(gameLobby.gameRoomEasy.length);
+			switch(player.difficulty){
+			case 'easy':
+				var room=_.find(gameLobby.gameRoomEasy, function(r){
+					console.log(r.status);
+					console.log(r.players.length);
+					console.log(r.max);
+					return r.status==0 && r.players.length<=r.max;
+				});
+				if(room==null){
+					//room Full! TODO: return room full msg.
+				}else{
+					room.add(player);
+					player.gameRoom=room;
+				};
+				gameLobby.freePlayers.splice(key,1);//remove the player after adding to a room.
+				break;
+			case 'normal':
+				break;
+			case 'hard':
+				break;
+			}
+		});
+		//if there is no free room, create one, assign the player to the room.
+	},
 
 };
-
-var gameRoom={
-	players:[],
-	quests:[]
+for (var i=0;i<10;i++){
+	gameLobby.gameRoomEasy.push(new gameRoom('easy'));
+	gameLobby.gameRoomNormal.push(new gameRoom('normal'));
+	gameLobby.gameRoomHard.push(new gameRoom('hard'));
+};
+function gameRoom(difficulty){
+	this.status=0; //0:awaiting joining, 1: started 
+	this.max=6;
+	this.maxTime=60;
+	this.gameTime=this.maxTime;
+	this.players=[];
+	this.quests=[];
+	this.difficulty=difficulty;
+	this.add=function(player){
+		console.log(player.name+' added into room.');
+		_.each(this.players, function(p, key){
+			p.socket.emit('playerJoined', {id:player.id,name:player.name});
+			player.socket.emit('playerJoined',{id:p.id,name:p.name});
+		});
+		this.players.push(player);
+		console.log('Total players in the room: '+this.players.length);
+	};
+	this.removePlayer=function(player){
+		//announce to the other players in the same room
+		_.each(this.players, function(p, key){
+			if(p.id==player.id){
+				p.gameRoom.players.splice(key,1);
+			}else{
+				p.socket.emit('playerLeft', {id:player.id});
+			}
+		});
+		console.log('Total players in the room: '+this.players.length);
+	};
+	//prepare to start the game. Called whenever a player clicks "ready"
+	this.prepareStartGame=function(){
+		//go over each player, check the ready status. if everyone is ready, start the game
+		var allReady=true;
+		for(var i=0;i<this.players.length;i++){
+			if(this.players[i].ready==false){
+				allReady=false;
+				break;
+			}
+		}
+		if(allReady){
+			this.gameStart();
+		}
+	};
+	//start the game!
+	this.gameStart=function(){
+		this.status=1; //gameRoom status = started
+		//tell everyone the game is started, client start count down
+		_.each(this.players, function(p,key){
+			p.socket.emit('gameStarted',{});
+		});
+		//after 3 seconds, send every player a quest
+		setTimeout(function(gameRoom){
+			_.each(gameRoom.players, function(p,key){
+				p.socket.emit('quest',{quest:gameRoom.getQuest(p)});
+			});
+			//finish the game in gameRoom.gameTime seconds;
+			var timerID=setInterval(function(gameRoom){
+				gameRoom.gameTime--;
+				_.each(gameRoom.players, function(p, key){
+					p.socket.emit('timeLeft', {current:gameRoom.gameTime, maxTime:gameRoom.maxTime})
+				});
+				if(gameRoom.gameTime<=0){
+					gameRoom.gameOver();
+					clearInterval(timerID);
+				};
+			}, 1000, gameRoom);
+		}, 3000, this);
+		
+	};
+	this.gameOver=function(){
+		this.status=0;
+		this.quests=[];
+		this.gameTime=this.maxTime;
+		_.each(this.players, function(p,key){
+			p.questIndex=0;
+			p.ready=false;
+			p.correctCount=0;
+			p.socket.emit('gameEnded', {});
+		});
+	};
+	//generates quests for the room.
+	this.getQuest=function(player){
+		//if there is enough quests in the quest queue, return the one in the queue
+		if(player.questIndex<this.quests.length){
+			return this.quests[player.questIndex++].quest;
+		}else{
+			//if there is not enough quest in the queue, generate one.
+			var index=this.quests.push(generateQuest('operations',this.difficulty));
+			player.questIndex++;
+			return this.quests[index-1].quest;
+		};
+	};
+	this.announceStatus=function(player, isCorrect){
+		_.each(this.players, function(p,key){
+			//send current player status to other players
+			if(p.id!=player.id){
+				p.socket.emit("playerStatus", {id:player.id, isCorrect:isCorrect, quest:player.gameRoom.quests[player.questIndex-1].quest, correctCount:player.correctCount});
+			};
+		});
+	};
 };
 
-var player={
-	name:null,
-	id:null,
-	gameRoom:null,
+player=function(name, id, socket, difficulty){
+	this.name=name;
+	this.id=id;
+	this.socket=socket;
+	this.difficulty=difficulty;
+	this.socket.emit('joinedRoom');
+	this.ready=false;
+	this.gameRoom=null;
+	this.questIndex=0;
+	this.correctCount=0;
 };
 
 generateQuest=function(operations, difficulty){
--- NodeExpressFoo/public/javascripts/client.js
+++ NodeExpressFoo/public/javascripts/client.js
@@ -1,10 +1,10 @@
 var socket;
-var gameState=0;//0:idle,1:countdown,2:gamestarted
+var gameState=0;//0:idle,1:countdown,2:gamestarted, 3:idleInRoom, 4: idleReady
 var currentQuest;
 var tick;
 var gameLength=10;
 $(document).ready(function(){
-	socket=io.connect('http://localhost');
+	socket=io.connect(window.location.host);
 	socket.on('connection', function(data){
 		if(data.msg==='pong'){
 			console.log('Server ready');
@@ -13,78 +13,94 @@ $(document).ready(function(){
 	});
 	socket.on('quest', function(data){
 		console.log(data);
+		gameState=2;
 		currentQuest=data;
-		if(gameState==0){
-			gameState=1;
-			$('#arena').fadeOut(500, function(){
-				var i=3;
-				$('#arena').html('<h1>'+i+'</h1>');
-				$('#arena').fadeIn(500);
-				tick=setInterval(function(){
-					if(i==1){
-						gameState=2;
-						clearInterval(tick);
-						populateArena();
-						tick=setInterval(function(){
-							$('#pbar').css('width',$('#pbar').width()+$('#pbar').parent().width()/gameLength);
-							if($('#pbar').width()>=$('#pbar').parent().width()){
-								clearInterval(tick);
-								gameOver();
-							}
-						},1000);
-					}else{
-						i--;
-						$('#arena').html('<h1>'+i+'</h1>');
-					}
-				},1000);
-				
-			});
-		}else if(gameState==2){
-			populateArena();
-			
-		}
+		populateArena();
 	});
 	socket.on('result', function(data){
 		console.log(data);
 		if(data.cheated){
-			$('#arena').html('<h1>Math Quest Heroes wouldn\'t cheat...</h1><button id="startGame" type="submit" class="btn btn-primary btn-large span2">Do it right this time <i class="icon-pencil icon-white"></i></button>');
+			$('#arena').html('<h1>Math Quest Heroes wouldn\'t cheat...</h1><button id="mainButton" type="submit" class="btn btn-primary btn-large span2">Do it right this time <i class="icon-pencil icon-white"></i></button>');
 		}else{
-			$('#arena').html('<h1>You scored: '+data.correctCount+'</h1><button id="startGame" type="submit" class="btn btn-primary btn-large span2">Play Again <i class="icon-pencil icon-white"></i></button>');
+			$('#arena').html('<h1>You scored: '+data.correctCount+'</h1><button id="mainButton" type="submit" class="btn btn-primary btn-large span2">Play Again <i class="icon-pencil icon-white"></i></button>');
 		}
 		$('#pbar').width(0);
-		$('#startGame').click(function(){
-			
-			if(socket){
-				socket.emit('newGame',{
-					operations:$('input[name=operations]:checked', '#settingsForm').val(),
-					difficulty:$('input[name=difficulty]:checked', '#settingsForm').val(),
-					competeMode:$('input[name=mode]','#settingsForm').prop('checked'),
-					playerName:$('input[name=playerName','#settingsForm').val(),
-					gameLength:gameLength
-				});
-			}
-		});
+		initStartButton();
+	});
+	
+	/**
+	 * Multiplayer mode
+	 * Buttons (emits): Join a room -> Ready / Leave
+	 * States (on): 
+	 * 	Joined a room, 
+	 * 	Player Joined, 
+	 * 	Player Ready,
+	 * 	Player left, 
+	 * 	Game Started, 
+	 * 	Player Scored, 
+	 * 	Game Ended.  
+	 *  
+	 */
+	
+	socket.on('joinedRoom', function(){
+		multiPlayer.joinedRoom();
+	});
+	socket.on('playerJoined', function(player){
+		console.log(player);
+		multiPlayer.addPlayer(player);
+	});
+	socket.on('playerLeft', function(player){
+		multiPlayer.removePlayer(player);
+	});
+	socket.on('gameStarted', function(data){
+		multiPlayer.gameStarted(data);
+	});
+	socket.on('playerStatus', function(player){
+		//other player answered quest, server sent status
+		multiPlayer.playerStatus(player);
+	});
+	socket.on('timeLeft', function(time){
+		//other player answered quest, server sent status
+		multiPlayer.updateTime(time.current, time.maxTime);
+	});
+	socket.on('gameEnded', function(data){
+		multiPlayer.gameEnded(data);
+	});
+	socket.on('playerReady', function(data){
+		multiPlayer.playerReady(data);
 	});
 	$('input[name=mode]','#settingsForm').click(function(){
 		if($(this).prop('checked')){
-			$('#startGame').html('Join a game');
+			$('#mainButton').html('Join a game');
 		}else{
-			$('#startGame').html('Start<i class="icon-pencil icon-white"></i>');
+			$('#mainButton').html('Start<i class="icon-pencil icon-white"></i>');
 		}
 		
 	});
+	
+	
 });
 
+$(window).bind('beforeunload',function(){
+	if(socket){
+		socket.emit('leaveRoom', {});
+	};
+});
 
 reset=function(){
 	gameState=0;
-	$('#arena').html('<h1>math-quest</h1><p>Practice your math skills and compete with others!</p><button id="startGame" type="submit" class="btn btn-primary btn-large span2">Start<i class="icon-pencil icon-white"></i></button>');
-	$('#startGame').click(function(){
+	$('#arena').html('<h1>math-quest</h1><p>Practice your math skills and compete with others!</p><button id="mainButton" type="submit" class="btn btn-primary btn-large span2">Start<i class="icon-pencil icon-white"></i></button>');
+	initStartButton();
+	$('#pbar').width(0);
+	
+};
+
+initStartButton=function(){
+	$('#mainButton').unbind("click").click(function(){
 		if($('input[name=mode]','#settingsForm').prop('checked')){
-			var player={name:'playerName',id:Math.random()*100};
-			multiPlayer.addPlayer(player);
+			multiPlayer.join();
 		}else{
-			if(socket){
+			if(socket && gameState==0){
 				socket.emit('newGame',{
 					operations:$('input[name=operations]:checked', '#settingsForm').val(),
 					difficulty:$('input[name=difficulty]:checked', '#settingsForm').val(),
@@ -92,10 +108,11 @@ reset=function(){
 					playerName:$('input[name=playerName]','#settingsForm').val(),
 					gameLength:gameLength
 				});
-			};
+			}else if(socket&& gameState==3){
+				multiPlayer.ready();
+			}
 		};
 	});
-	$('#pbar').width(0);
 };
 
 populateArena=function(){
@@ -119,11 +136,120 @@ gameOver=function(){
 	gameState=0;
 };
 
+/**
+ * Multiplayer mode
+ * Buttons (emits): Join a room -> Ready / Leave
+ * States (on): 
+ * 	Joined a room, 
+ * 	Player Joined, 
+ * 	Player Ready,
+ * 	Player left, 
+ * 	Game Started, 
+ * 	Player Scored, 
+ * 	Game Ended.  
+ *  
+ */
+
 var multiPlayer={
+	players:[],
+	join:function(){
+		//Join a room
+		if(socket){
+			socket.emit('joinRoom', {name:$('input[name=playerName]','#settingsForm').val().length==0?'Player':$('input[name=playerName]','#settingsForm').val(),
+				difficulty:$('input[name=difficulty]:checked', '#settingsForm').val()});
+			$('#mainButton').html('Joining a room');
+		};
+	},
+	ready:function(){
+		if(socket){
+			socket.emit('ready');
+			$('#mainButton').html('Waiting for everyone to be ready');
+			$('#mainButton').prop('disabled',true);
+		};
+	},
+	playerReady:function(player){
+		$('#pid'+player.id).css('background-color','green');
+	},
+	leaveRoom:function(){
+		
+		if(socket){
+			socket.emit('leaveRoom', {});
+			$('#secButton').detach();
+			$('#mainButton').html('Join a game');
+			initStartButton();
+		};
+		$('#otherPlayers').empty();
+	},
+	joinedRoom:function(players){
+		//joined a room
+		gameState=3;
+		$('#mainButton').html('Click to be Ready');
+		$('#mainButton').unbind("click").click(function(){
+			multiPlayer.ready();
+		});
+		$('#mainButton').after('<button id="secButton" class="btn btn-primary btn-large span2">Leave the room.</button>');
+		$('#secButton').unbind("click").click(function(){
+			multiPlayer.leaveRoom();
+			gameState=0;
+		});
+		
+	},
+	gameStarted:function(data){
+		gameState=1;
+		//prepare arena for quests, count down and wait for the first quest.
+		$('#arena').fadeOut(500, function(){
+			var i=3; //count down
+			$('#arena').html('<h1>'+i+'</h1>');
+			$('#arena').fadeIn(500);
+			tick=setInterval(function(){
+				if(i==1){
+					gameState=2;
+					clearInterval(tick);
+//					populateArena();
+//					tick=setInterval(function(){
+//						$('#pbar').css('width',$('#pbar').width()+$('#pbar').parent().width()/gameLength);
+//						if($('#pbar').width()>=$('#pbar').parent().width()){
+//							clearInterval(tick);
+//							gameOver();
+//						}
+//					},1000);
+				}else{
+					i--;
+					$('#arena').html('<h1>'+i+'</h1>');
+				}
+			},1000);
+			
+		});
+	},
+	playerStatus:function(player){
+		//{id, isCorrect, quest, correctCount}
+		$('#pid'+player.id+'_quest').html(player.quest);
+		$('#pid'+player.id+'_score').html('Score '+player.correctCount);
+		$('#pid'+player.id).css('background-color',player.isCorrect?'green':'red');
+		$('#pid'+player.id).stop().animate({backgroundColor:'#fff'},1000);
+	},
+	updateTime:function(current, max){
+		$('#pbar').css('width',$('#pbar').parent().width()*current/max);
+	},
 	addPlayer: function(player){
-		$('#otherPlayers').append('<div id="pid'+player.id+'" class="span4"><h2>'+player.name+'</h2></div>');
+		console.log(player);
+		this.players.push(player);
+		$('#otherPlayers').append('<div id="pid'+player.id+'" class="span4">'+
+				'<h2>'+player.name+'</h2><div id="pid'+player.id+'_quest"></div>'+
+				'<div id="pid'+player.id+'_score">Score 0</div></div>');
 	}, 
 	removePlayer: function(player){
-		
+		console.log(player.id+' left');
+		$.each(this.players, function(p, key){
+			if(p.id===player.id){
+				this.players.splice(key,1);
+			};
+		});
+		$('#pid'+player.id).detach();
+	},
+	gameEnded:function(data){
+		$('#arena').html('<h1>Game Ended</h1>');
 	}
 };
+
+
